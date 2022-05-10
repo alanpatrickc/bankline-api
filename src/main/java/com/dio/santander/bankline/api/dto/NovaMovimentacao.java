7f63d263f1df61ff6632bf363ae8f9690b734a80
alanpatrickc <alanpatrickc@hotmail.com>
2022-05-10 13:26:49 -0300

    subindo movimentacao

---
+++ src/main/java/com/dio/santander/bankline/api/dto/NovaMovimentacao.java
@@ -0,0 +1,47 @@
+package com.dio.santander.bankline.api.dto;
+
+import com.dio.santander.bankline.api.model.MovimentacaoTipo;
+
+public class NovaMovimentacao {
+
+	private String descricao;
+	private double valor;
+	
+	private MovimentacaoTipo tipo;
+	
+	private Integer idConta;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public MovimentacaoTipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(MovimentacaoTipo tipo) {
+		this.tipo = tipo;
+	}
+
+	public Integer getIdConta() {
+		return idConta;
+	}
+
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
+	
+	
+}
